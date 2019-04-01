2eed6684cdb06b0ee82295a7de373d6dabafec7e
hp <hp@DESKTOP-D7RFM1B>
2019-04-01 13:06:34 +0530

    modification dev1

--- gitproj1/src/com/nt/test/Test.java
+++ gitproj1/src/com/nt/test/Test.java
@@ -1,10 +1,32 @@
 package com.nt.test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Test {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 System.out.println("gud mrng");
+try {
+	Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","scott","tiger");
+	//create jdbc statement
+	Statement st=con.createStatement();
+	//execute query and process the result object
+	ResultSet rs=st.executeQuery("select * from emp");
+	while(rs.next()!=false)
+	{
+		System.out.println(rs.getInt(1)+""+rs.getString(2)+""+rs.getString(3));
+		
+		//sony ajay sow mahi
+	}
+}
+catch(SQLException se) {
+se.printStackTrace();
+}
 	}
 
 }
